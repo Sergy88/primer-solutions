cb59d323c28d87ec380a80d472cea0b489492932
Sergey <mrserjy@gmail.com>
2021-03-16 17:12:58 +0300

    new ex

---
+++ chap3/ex04/src.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+const int HOURS_DAY = 24;
+const int MINS_HOUR = 60;
+const int SECS_MIN = 60;
+
+int main()
+{
+	int seconds;
+	int secs;
+	int mins;
+	int hours;
+	int days;
+	using namespace std;
+	cout << "Please enter the number of seconds:__\b\b";
+	cin >> seconds;
+	days = seconds /  (HOURS_DAY * MINS_HOUR * SECS_MIN);
+	seconds = seconds %  (HOURS_DAY * MINS_HOUR * SECS_MIN);
+	hours = seconds / (MINS_HOUR * SECS_MIN);
+	seconds = seconds %  (MINS_HOUR * SECS_MIN);
+	mins = seconds / SECS_MIN;
+	secs = seconds % SECS_MIN;
+	cout<<seconds<<" seconds = "<< days <<" days, "<<hours<<" hours, "<< mins << " minutes, "<< secs<<" seconds. \n";
+}
